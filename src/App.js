86b60b7392622ffc9c114648e48b160f451f3de7
lsd <337253633@qq.com>
2024-07-05 10:50:39 +0800

    测试

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Avatar from './components/Avatar';
 
 function App() {
   return (
     <div className="App">
+      <Avatar></Avatar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Avatar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import AvatarImg from '../assets/dog.jpg'
+export default function Avatar() {
+  return (
+    <div>
+      <img src={AvatarImg} alt="" />
+    </div>
+  )
+}
